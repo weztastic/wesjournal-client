1f70043a9f3053c89c43ae3d81826d61b6d80b36
weztastic <wesseldehaas@gmail.com>
2020-04-27 11:14:51 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,19 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "Ywesnotes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://at0592ws01.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_Gy9h2eJUh",
+      APP_CLIENT_ID: "1eg4hnqvel4htub35nstpmhr6",
+      IDENTITY_POOL_ID: "eu-central-1:927f5f17-2ff6-4cbe-a511-1a3879f96c1f"
+    }
+  };
+
+
+
