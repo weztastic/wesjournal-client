5a8044a97789a954f6753232a15b8af094ee6de3
weztastic <wesseldehaas@gmail.com>
2020-04-29 21:06:14 +0200

    Building our React app

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">WesNotes</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
